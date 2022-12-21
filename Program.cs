9201eabd22eaf76ee7df56e528d84e65ed0d8b68
abdelrahaman2050 <121178371+abdelrahaman2050@users.noreply.github.com>
2022-12-21 17:07:47 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,13 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1PrimeProject
+namespace _1PerfectProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" please enter the first number: ");
+            int firstnumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(" please enter the second number: ");
+            int secondnumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+            int sum;
+            for (int x = firstnumber; firstnumber < secondnumber; firstnumber++)
+            {
+                sum = 0;
+                for (int i = 1; i <= firstnumber / 2; i++)
+                {
+                    if (firstnumber % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                if (sum == firstnumber)
+                {
+                    Console.WriteLine(" this number is perfect number :" + firstnumber);
+                }
 
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Developed by : ");
+            Console.WriteLine("Abdelahman Elshrabasi\nYousif Emad\nBasant Omar");
+            Console.ReadKey();
         }
     }
 }
